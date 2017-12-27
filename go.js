f3ad780db4a8c6189c27ae2277bfe52911f81790
dcaix <dcaix89@163.com>
2017-12-27 23:01:21 +0800

    Delete go.js

--- go.js
+++
@@ -1,9 +0,0 @@
-angular.module("todoList",[])
-    .controller("TaskCtrl",function ($scope) {
-        $scope.task = "";
-        $scope.tasks = [];
-        $scope.add = function () {
-            $scope.tasks.push($scope.task)
-        }
-    })
-;
